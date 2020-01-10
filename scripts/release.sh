0d3532bdd4ccad311455825b0a7fee8f01eac596
Ingo Bürk <admin@airblader.de>
2020-01-10 09:19:52 +0100

    chore: proper registry
    Signed-off-by: Ingo Bürk <admin@airblader.de>

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,5 +5,5 @@ set -e -o pipefail
 rm -rf dist/ngx-structurals
 lerna version
 ng build ngx-structurals
-cd dist/ngx-structurals && npm publish --access public ; cd -
+cd dist/ngx-structurals && npm publish --access public --registry https://registry.npmjs.org/ ; cd -
 git push --tags
