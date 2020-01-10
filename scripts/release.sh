725362dd8f718057e58f862553bc066ed17ea5b6
Ingo Bürk <admin@airblader.de>
2020-01-10 09:20:41 +0100

    chore: push
    Signed-off-by: Ingo Bürk <admin@airblader.de>

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,4 +6,6 @@ rm -rf dist/ngx-structurals
 lerna version
 ng build ngx-structurals
 cd dist/ngx-structurals && npm publish --access public --registry https://registry.npmjs.org/ ; cd -
+
+git push
 git push --tags
